5c9025fd58a0b72c63404c5698b6c8582c596506
Wajahat <wajahath533@gmail.com>
2024-05-30 15:52:59 +0500

    Committing changes to home/views.py, about.html, contact.html, and prediction.html

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import joblib
+import os
+import sklearn
 
+# Construct the file path
+model_path = os.path.join(os.path.dirname(__file__), '../static/random_forest_regressor')
+
+# Load the model
+model = joblib.load(model_path)
 
 def index(request):
     return render(request, "index.html")
@@ -8,8 +16,24 @@ def about(request):
     return render(request, "about.html")
 
 def prediction(request):
-    
-  
+    if request.method == "POST":
+        age = int(request.POST.get("age"))
+        gender = int(request.POST.get("gender"))
+        bmi = float(request.POST.get("bmi"))  # BMI is typically a float
+        children = int(request.POST.get("children"))
+        smoker = int(request.POST.get("smoker"))
+        region = int(request.POST.get("region"))
+        print(age, gender, bmi, children, smoker, region)
+
+        pred = model.predict([[age, gender, bmi, children, smoker, region]])
+
+        output = {
+            'output': pred[0]  # pred is an array, return the first element
+        }
+        return render(request, "prediction.html", output)
+    else:
+        import sklearn
+        print(sklearn.__version__)
         return render(request, "prediction.html")
 
 def contact(request):
